6f1256cab8d22cd663dfcc5c55181a6a8183033f
zultriyadi <zuldbc11@gmail.com>
2026-03-25 15:26:12 +0800

    fix storage link

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,5 +30,9 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
+
+        if (!file_exists(public_path('storage'))) {
+        Artisan::call('storage:link');
+    }
     }
 }
