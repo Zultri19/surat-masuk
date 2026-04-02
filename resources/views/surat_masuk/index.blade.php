488f2e5f5ec5631eb9be2b12d13d0aad9773a14a
zultriyadi <zuldbc11@gmail.com>
2026-04-02 13:58:12 +0800

    update

--- resources/views/surat_masuk/index.blade.php
+++ resources/views/surat_masuk/index.blade.php
@@ -48,7 +48,7 @@ use Illuminate\Support\Str;
                     <td>{{ $s->tanggal_surat->format('d-m-Y') }}</td>
                     <td>{{ $s->nomor_surat }}</td>
                     <td title="{{ $s->perihal }}">{{ Str::limit($s->perihal, 50) }}</td>
-                    <td title="{{ $s->file }}">  <a href="{{ Str::limit($s->file, 5) }}"
+                    <td title="{{ $s->file }}">  <a href="{{ Str::limit($s->file, 50) }}"
                                 class="btn btn-info btn-sm"
                                 title="Preview">
                                 <i class="fa-solid fa-eye"></i>
